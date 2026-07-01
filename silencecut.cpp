e86b9f48785d9c6b302a737aee48e0b9b407374e
Ace_Tan <18611708782@163.com>
2026-07-01 23:55:15 +0800

    select_audio

--- silencecut.cpp
+++ silencecut.cpp
@@ -1,5 +1,7 @@
 #include "silencecut.h"
 #include "ui_silencecut.h"
+#include <QFileDialog>
+#include <QStandardPaths>
 
 silencecut::silencecut(QWidget *parent)
     : QWidget(parent)
@@ -17,5 +19,22 @@ silencecut::~silencecut()
 void silencecut::on_audio_select_botton_clicked()
 {
 
+    QString filePath = QFileDialog::getOpenFileName(
+        this,
+        tr("选择要切分的音频文件"),
+        "",
+        tr("音频文件 (*.mp3 *.wav *.m4a *.flac);;所有文件 (*.*)")//音频格式可选，其他为灰
+        );
+
+    if (!filePath.isEmpty()) {
+        // 显示输入路径
+        ui->input_address->setText(filePath);
+
+        // 在TextLog中打印日志
+        ui->TextLog->append(QString("<font color='#007AFF'><b>[输入]</b></font> 已选择音频：%1").arg(filePath));
+    }
+    else{
+        ui->TextLog->append(QString("请选择音频"));
+    }
 }
 
